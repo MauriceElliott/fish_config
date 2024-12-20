f4929baa9d7276fd5540cb54ef83b8739fbefb0e
MauriceElliott <mauricemelliott@gmail.com>
2024-12-20 00:56:52 +0000

    initial commit

---
+++ config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+zoxide init fish | source
+
+alias cd="z"
+alias ls='eza --icons=always'
+alias ll="ls -la"
+
+#directory vars
+set GWD '/Users/mauriceelliott/Library/Application Support/Picotron/drive/desktop/projects/gut_wound'
+
+fzf --fish | source
+
+set fish_greeting "Hello Maurice, what are we working on today?"
+
+clear && neofetch
