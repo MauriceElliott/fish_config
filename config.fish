0440d427e1eee4da2d30f39218e3f19a10336aed
MauriceElliott <mauricemelliott@gmail.com>
2025-10-16 22:44:21 +0100

    added zellij to workflow

--- config.fish
+++ config.fish
@@ -3,25 +3,34 @@ set -g fish_history_max 1000000
 set -g fish_history_save_on_exit 1
 
 if status is-interactive
-    zoxide init fish | source
+    if not set -q ZELLIJ
+        zellij attach --create main
+    else
+        zoxide init fish | source
 
-    alias cd="z"
-    alias ls='eza --icons=always'
-    alias ll="ls -la"
+        alias cd="z"
+        alias ls='eza --icons=always'
+        alias ll="ls -la"
 
-    fzf --fish | source
+        fzf --fish | source
 
-    set fish_greeting ""
+        set fish_greeting ""
 
-    sleep 0.1 #fixes a load of issues with fast fetch, not really sure why, but it seems to be consistent across terminals and OS's that adding this solves a lot of issues
+        sleep 0.1 #fixes a load of issues with fast fetch, not really sure why, but it seems to be consistent across terminals and OS's that adding this solves a lot of issues
 
-    clear && fastfetch
-    echo ""
+        clear && fastfetch
+        echo ""
 
-    if test -f ~/.config/fish/local_variables.fish
-        source ~/.config/fish/local_variables.fish
+        if test -f ~/.config/fish/local_variables.fish
+            source ~/.config/fish/local_variables.fish
+        end
+
+        export COLORTERM=truecolor
+
+        jdi
+        echo ""
     end
-
-    export COLORTERM=truecolor
-    #    boot
 end
+
+# opencode
+fish_add_path /home/maurice/.opencode/bin
