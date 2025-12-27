fb21c68473eb5c6333809bd276efe354273a4893
MauriceElliott <mauricemelliott@gmail.com>
2025-12-27 23:18:13 +0000

    Added PlaydateSDK to path for Gentoo Computer.

--- _eighthcircle.fish
+++ _eighthcircle.fish
@@ -3,4 +3,7 @@ if test (hostname) = eighthcircle
     source "/home/maurice/.local/share/swiftly/env.fish"
     # Added by swiftly
     source "/root/.local/share/swiftly/env.fish"
+    # Playdate SDK
+    set PLAYDATE_SDK_PATH ~/PlaydateSDK
+    fish_add_path $PLAYDATE_SDK_PATH/bin
 end
