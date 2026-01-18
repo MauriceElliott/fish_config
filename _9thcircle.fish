e033cd0f66aa037903c7d4ea4c6178c9f62edf86
MauriceElliott <mauricemelliott@gmail.com>
2026-01-18 22:41:59 +0000

    Updating for both mac and linux machines with playdate var

--- _9thcircle.fish
+++ _9thcircle.fish
@@ -1,4 +1,5 @@
 if test (hostname) = ninthCircle
     set PATH $PATH ~/.local/bin
-    set PLAYDATE_SDK_PATH ~/Developer/PlaydateSDK
+    set -gx PLAYDATE_SDK_PATH ~/Developer/PlaydateSDK
+    fish_add_path $PLAYDATE_SDK_PATH/bin
 end
--- _eighthcircle.fish
+++ _eighthcircle.fish
@@ -4,6 +4,6 @@ if test (hostname) = eighthcircle
     # Added by swiftly
     source "/root/.local/share/swiftly/env.fish"
     # Playdate SDK
-    set PLAYDATE_SDK_PATH ~/PlaydateSDK
+    set -gx PLAYDATE_SDK_PATH ~/PlaydateSDK
     fish_add_path $PLAYDATE_SDK_PATH/bin
 end
