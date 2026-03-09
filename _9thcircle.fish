5b7d09008b222db686e28ac36ade8cd4837f6576
MauriceElliott <mauricemelliott@gmail.com>
2026-03-09 21:34:59 +0000

    added odin language server, can stay in repos.

--- _9thcircle.fish
+++ _9thcircle.fish
@@ -1,5 +1,6 @@
 if test (hostname) = ninthCircle
     set PATH $PATH ~/.local/bin
+    set PATH $PATH ~/repos/ols
     set -gx PLAYDATE_SDK_PATH ~/Developer/PlaydateSDK
     fish_add_path $PLAYDATE_SDK_PATH/bin
     set -gx PDSIM $PLAYDATE_SDK_PATH/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator
