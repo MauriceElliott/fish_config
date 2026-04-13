0a53dd6287812372b58afb0015aaa9f1abd342a1
MauriceElliott <mauricemelliott@gmail.com>
2026-04-13 22:53:51 +0100

    added bun

--- _9thcircle.fish
+++ _9thcircle.fish
@@ -4,4 +4,7 @@ if test (hostname) = ninthCircle
     set -gx PLAYDATE_SDK_PATH ~/Developer/PlaydateSDK
     fish_add_path $PLAYDATE_SDK_PATH/bin
     set -gx PDSIM $PLAYDATE_SDK_PATH/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator
+    # bun
+    set --export BUN_INSTALL "$HOME/.bun"
+    set --export PATH $BUN_INSTALL/bin $PATH
 end
