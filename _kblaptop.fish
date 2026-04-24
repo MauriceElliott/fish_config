07624dabe50f5ec10ac21b7cb7b1d5c3eb8a949b
MauriceElliott <maurice.elliott@kbtech.co.uk>
2026-04-24 10:40:36 +0100

    Minor update to kb_laptop vars

--- _kblaptop.fish
+++ _kblaptop.fish
@@ -2,5 +2,6 @@ if test (hostname) = KB-GH85FB4
     # Add taclaptop-specific configuration here
     set PATH $PATH /root/go/bin
     set PATH $PATH /home/mauricelliott/.local/bin
+    set PATH $PATH ~/.local/bin
     alias hx helix
 end
